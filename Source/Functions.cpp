f5049170739ee31b2bbaaf0be916474ba71cb1ff
Eloquence4 <zelin11@live.com>
2017-01-13 23:37:59 +0200

    Only 1 function left to implement

--- Source/Functions.cpp
+++ Source/Functions.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "../Header/Functions.h"
 
 void add(BinaryTree<DataNode>& tree)
@@ -7,4 +8,68 @@ void add(BinaryTree<DataNode>& tree)
     __int32 dataSize = strlen(data);
 
     tree.Add(DataNode(key, std::move(data), dataSize));
+}
+
+void remove(BinaryTree<DataNode>& tree)
+{
+    __int32 key = read_key();
+    char* data = read_data();
+    __int32 dataSize = strlen(data);
+
+    if(tree.Remove(DataNode(key, std::move(data), dataSize)))
+        std::cout << "true\n";
+    else
+        std::cout << "false\n";
+}
+
+void remove_all(BinaryTree<DataNode>& tree)
+{
+    __int32 key = read_key();
+
+    std::cout << remove_all(tree, key) << '\n';
+}
+
+void search(BinaryTree<DataNode>& tree)
+{
+    __int32 key = read_key();
+    char* data = read_data();
+    __int32 dataSize = strlen(data);
+
+    try
+    {
+        tree.Search(DataNode(key, std::move(data), dataSize));
+        std::cout << "true\n";
+    }
+    catch(TreeExceptions& err)
+    {
+        if(err == NOT_FOUND)
+            std::cout << "false\n";
+        else
+            throw err;
+    }
+}
+
+
+
+__int32 read_key()
+{
+    __int32 key = 0;
+    std::cin >> key;
+
+    return key;
+}
+
+char* read_data()
+{
+    char buffer[buffer_max_size+1] = {'\0'};
+
+    std::cin.getline(buffer, buffer_max_size);
+
+    size_t len = strlen(buffer);
+
+    char* data = new char[len+1];
+
+    strcpy(data, buffer);
+
+    return data;
 }
